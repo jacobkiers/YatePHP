245093b0d463af056055652a497e35f3b1ffcdc9
Jacob Kiers <jacob@jacobkiers.net>
2010-12-11 14:20:46 +0100

    Added Message, the concrete implementation of AbstractMessage

---
+++ library/Yate/Core/Message/Message.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * YatePHP
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to develop@jacobkiers.net so we can send you a copy immediately.
+ *
+ * @copyright   Copyright (c) 2010 Jacob Kiers
+ * @license     New BSD License
+ *
+ * @category    Yate
+ * @package     Yate_Core
+ * @subpackage  Message
+ */
+
+namespace Yate\Core\Message;
+
+/**
+ * Represents a Yate message
+ *
+ * @category    Yate
+ * @package     Yate_Core
+ *
+ * @copyright   Copyright (c) 2010 Jacob Kiers
+ * @license     New BSD License
+ *
+ * @since       0.1
+ */
+class Message extends AbstractMessage
+{}
+
+/* vi: set softtabstop=4 shiftwidth=4 expandtab: */
