810bd1a1aa901c96445e9c229d90e60d7b69a981
Jacob Kiers <jacob@jacobkiers.net>
2011-03-03 22:00:37 +0100

    Updated notification of licence in all source files

--- library/Yate/Core/Message/Message.php
+++ library/Yate/Core/Message/Message.php
@@ -4,8 +4,8 @@
  *
  * LICENSE
  *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
+ * This source file is subject to the license that is bundled
+ * with this package in the file LICENSE.TXT.
  *
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
